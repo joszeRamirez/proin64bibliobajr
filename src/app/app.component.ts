2156592efd2fda974ad04fd516a75ab0d210e535
BryanAv7 <bavilag@est.ups.edu.ec>
2024-05-30 05:06:28 -0500

    Editar Perfil y Lista de Usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -20,7 +20,7 @@ export class AppComponent {
   correo = 'asistencia@bibliobajr.com'
   obras = 0
 
-  constructor(private router: Router,private firestore: AngularFirestore,private libroService:LibroService){}
+  constructor(private router: Router,private libroService:LibroService){}
 
   //async getCantidadDatos() {
     //try {
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { getAuth, createUserWithEmailAndPassword } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { User } from '../../domain/user';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registro',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss']
 })
 export class RegistroComponent {
   constructor(private router: Router) {}
 
-  register(email: string, user: string, password: string) {
+  user: User = new User();
+
+  register() {
     const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
+    createUserWithEmailAndPassword(auth, this.user.correo, this.user.passwo)
       .then((userCredential) => {
-        
+
         // Registered
         const user = userCredential.user;
         console.log('Registration successful', user);
